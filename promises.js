1efd4a549fb05e822f50dcb9da56188c59dcf66e
BROSSETTE Clement <clement.brossette@ynov.com>
2018-05-31 11:00:42 +0200

    finish

--- promises.js
+++ promises.js
@@ -1,4 +1,4 @@
-function superCompress(input) {
+/*function superCompress(input) {
   return readFromCache(input)
     .then(
       result => cleanCacheMetadata(result),
@@ -15,4 +15,60 @@ function superCompress(input) {
       }
     )
     .then(content => compress(content));
+}
+
+-------------------------------------------------------------------------*/
+
+async function readFromCache(input)=>{
+    return new Promise((resolve) => {
+        readFromCache((input)=> {
+            resolve();
+        });
+    });
+}
+
+
+async function cleanCacheMetadata(something)=>{
+    return new Promise((resolve) => {
+        cleanCacheMetadata((something)=> {
+            resolve();
+        });
+    });
+}
+
+async function readFromFile(input)=>{
+    return new Promise((resolve) => {
+        readFromFile((input)=> {
+            resolve();
+        });
+    });
+}
+
+async function storeInCache(input, result)=>{
+    return new Promise((resolve) => {
+        storeInCache((input, result)=> {
+            resolve();
+        });
+    });
+}
+
+
+async function superCompress(input){
+    let content;
+    try{
+        const result = await readFromCache(input);
+        content = await cleanCacheMetadata(result);
+    }
+    catch (error)
+    {
+        if (error.code != 'NoCache') {
+            throw error;
+        }
+
+        const result = await readFromFile(input);
+        content = await storeInCache(input, result);
+    }
+
+    await compress(content);
+
 }
